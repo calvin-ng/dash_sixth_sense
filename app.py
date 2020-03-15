7338233a1fe84697405f466fa1e0b69907651e4e
calvin-ng <calvin-ng@live.com>
2020-03-15 22:57:36 +0000

    update heroku

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ mapbox_access_token = 'pk.eyJ1IjoiY2FsZGFzaHZpbm5nIiwiYSI6ImNqcGQzdXlndjAzbnkza3
 
 #dash app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
 server= app.server
 app.config.suppress_callback_exceptions = True
 
