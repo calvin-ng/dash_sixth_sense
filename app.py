7824ca92124856e1676a13c99fd6c0f72e085fac
calvin-ng <calvin-ng@live.com>
2020-03-25 16:51:32 +0800

    Edited case info, fixed array size 0 bug

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ body = html.Div(
             html.Div(
                 dcc.Graph(
                     id='mapgraph',
-                    clickData={'points': [{'customdata': '7425'}]},
+                    clickData={'points': [{'customdata': '1670'}]},
                     style={'width': '100%','padding': '0px'}
                 ),
                 className = 'col-lg-10'
@@ -55,7 +55,7 @@ body = html.Div(
 
             html.Div(
                 children=[
-                    html.Div('TOTAL CASES', style={'font-size':'16px', 'text-align':'left', 'color':'#756263', 'font-weight': 'bold', 'margin-left':'30px'}),
+                    html.Div('TOTAL CASES', style={'font-size':'24px', 'text-align':'left', 'color':'#756263', 'font-weight': 'bold', 'margin-left':'5px'}),
                     daq.LEDDisplay(
                         id='total_cases',
                         value= 0,
@@ -63,31 +63,12 @@ body = html.Div(
                         color="#756263",
                         backgroundColor="#FFFFFF",
                     ),
-                    html.Div(id='case_info'),
+                    html.Div('CASE INFORMATION', style={'font-size':'20px', 'text-align':'left', 'color':'#756263', 'font-weight': 'bold', 'margin-left':'5px'}),
+                    html.Div(id='case_info', style={'background':'#FFFFFF', 'padding':'10px', 'margin-top':'5px', 'font-size':'18px', 'color':'#000000'}),
                 ],
                 className = 'col-lg-2'
             )
-            # html.Div( #col-lg-3 div
-            #     children=[
-            #         dt.DataTable(
-            #             id='case_table',
-            #             columns=[{"name": i, "id": i} for i in df.columns],
-            #             data=df.to_dict('records'),
-            #             style_table={
-            #                 'maxHeight': '420px',
-            #                 'maxWidth':'300px',
-            #                 'overflowY': 'scroll',
-            #                 'overflowX': 'scroll'
-            #             },
-            #         ),
-            #
-            #         # html.Div( #div for the graph
-            #         #     dcc.Graph(id='CVgraph')
-            #         # ),
-            #     ],
-            #
-            #     className='col-lg-3',
-            # )
+
 
         ], style={"width":"100%"}), #END OF dbc.Row
 #================================================================================
@@ -118,27 +99,7 @@ body = html.Div(
                 className='col-lg-2',
                 style={'padding':'5px'}
             ),
-                #RADIO ITEM LIST
-                # dcc.RadioItems(
-                #     id='class-radio',
-                #     options=[
-                #         {'label': 'Infected', 'value': 'Infected'},
-                #         {'label': 'Not-Infected', 'value': 'Not Infected'},
-                #     ],
-                #     value='Infected'
-                # ),
 
-                # #CHECKLIST FOR CLASS
-                #     html.Div(
-                #         dcc.Checklist(
-                #             id='class-checklist',
-                #             options=[
-                #                 {'label': 'Infected', 'value': 'Infected'},
-                #                 {'label': 'Not-Infected', 'value': 'Not Infected'},
-                #             ],
-                #             value=['Infected']
-                #         ),
-                #     ), #END OF CHECKLIST FOR CLASS
         ], style={'padding': '50px'}),
 
         dbc.Row([
@@ -162,6 +123,8 @@ app.layout = html.Div(
 #======================================================================
 #============app callback and function declarations
 #======================================================================
+
+#UPDATE TOTAL CASES
 @app.callback(
     Output('total_cases', 'value'),
     [Input('date-slider', 'value'),
@@ -177,20 +140,28 @@ def update_total_cases(year, toggle):
 
     return dff.shape[0]
 
+#UPDATE CASE INFO
 @app.callback(
     Output('case_info', 'children'),
     [Input('mapgraph', 'clickData')]
     )
 def update_case_info(clickData):
-    dff = df[df['number'] == clickData['points'][0]['customdata']]
-    number = dff['number'].values[0]
-    user_id = dff['user id'].values[0]
-    date = str(dff['date'].values[0]) + "-" + str(dff['year'].values[0])
-    info =  ["Case #: {}".format(number), "\n", "User ID: {}".format(user_id), "\n", "Date: {}".format(date)]
-    return info
+    if (df[df['number'] == clickData['points'][0]['customdata']].shape[0]) == 0:
+        return "No case selected"
+    else:
+        dff = (df[df['number'] == clickData['points'][0]['customdata']])
+        number = dff['number'].values[0]
+        user_id = dff['user id'].values[0]
+        date = str(dff['date'].values[0]) + "-" + str(dff['year'].values[0])
+        classification = dff['classification'].values[0]
+        info =  ["Case #: {}".format(number), html.Br(),
+            "User ID: {}".format(user_id), html.Br(),
+            "Date: {}".format(date), html.Br(),
+            "Status: {}".format(classification)]
+        return info
 
 
-#Displaying output for toggle switch
+#UPDATE TOGGLE DISPLAY
 @app.callback(
     Output('class-toggle-output', 'children'),
     [Input('class-toggle', 'value')])
@@ -228,10 +199,7 @@ def update_map(year, toggle):
             visible=True,
             colorbar=dict(borderwidth=1, xpad=1, ypad=1, thickness=3)
         )
-    ],
-
-        )
-
+    ])
 
     # Layout and mapbox properties
     layout = go.Layout(
@@ -251,17 +219,172 @@ def update_map(year, toggle):
             't': 0,
             'r': 0
         },
-
-
     )
-
 
     return go.Figure(
             data = mapdata,
             layout = layout
+    )
+
+
+
+#YEAR GRAPH
+@app.callback(
+    Output('by_year', 'figure'),
+    [Input('date-slider', 'value')]
+)
+def by_year(year):
+    dff = df[(df['year'] >= year[0]) & (df['year'] <=year[1])]
+    # dfff = dff[dff['classification']=='empty']
+    classification =['Infected', 'Not Infected']
+    for classes in classification:
+    #     dfff = dfff.append(dff[dff['classification'] == classes], ignore_index=True)
+        dff_counted = dff.groupby(['year','classification'] , as_index=False).count()
+    dff_A = dff_counted[dff_counted['classification'] == 'Infected']['number']
+    dff_B = dff_counted[dff_counted['classification'] == 'Not Infected']['number']
+    y_min = dff_counted['number'].min()
+    y_max = dff_counted['number'].max()
+
+    data = go.Data([
+        go.Bar(
+            name='Infected',
+            x=np.arange(year[0], year[1]),
+            y=dff_A,
+            visible = True,
+            hoverlabel={
+                'bgcolor': '#FFF',
+            },
+            marker_color='#407438'
+        ),
+        go.Bar(
+            name='Not Infected',
+            # events qty
+            x=np.arange(year[0], year[1]),
+            # year
+            y=dff_B,
+            visible = True,
+            hoverlabel={
+                'bgcolor': '#FFF',
+            },
+            marker_color = '#65BC22'
+        ),
+
+    ])
+
+    layout = go.Layout(
+        xaxis={
+            #'autorange': True,
+            'color': '#000000',
+            'title': 'year',
+            'range': [year[0], year[1]],
+            'dtick': 1
+            },
+        yaxis={
+            #'autorange': True,
+            'color': '#000000',
+            'title': 'Number of Cases',
+            'range': [y_min, y_max],
+            #'dtick': 5
+            },
+        margin={
+            'l': 0,
+            'b': 0,
+            't': 0,
+            'r': 0
+            },
+            hovermode='closest',
+            paper_bgcolor='#F0E4E3',
+            plot_bgcolor='#F0E4E3',
+            autosize = True,
             )
 
+    return go.Figure(
+        data=data,  # 54b4e4
+        layout=layout
+        )
 
+#=================Commented, saved for later=============#######
+#####=============================================##############
+# RADIO ITEM LIST
+# dcc.RadioItems(
+#     id='class-radio',
+#     options=[
+#         {'label': 'Infected', 'value': 'Infected'},
+#         {'label': 'Not-Infected', 'value': 'Not Infected'},
+#     ],
+#     value='Infected'
+# ),
+#
+# #CHECKLIST FOR CLASS
+#     html.Div(
+#         dcc.Checklist(
+#             id='class-checklist',
+#             options=[
+#                 {'label': 'Infected', 'value': 'Infected'},
+#                 {'label': 'Not-Infected', 'value': 'Not Infected'},
+#             ],
+#             value=['Infected']
+#         ),
+#     ), #END OF CHECKLIST FOR CLASS
+#
+#             html.Div( #col-lg-3 div
+#                 children=[
+#                     dt.DataTable(
+#                         id='case_table',
+#                         columns=[{"name": i, "id": i} for i in df.columns],
+#                         data=df.to_dict('records'),
+#                         style_table={
+#                             'maxHeight': '420px',
+#                             'maxWidth':'300px',
+#                             'overflowY': 'scroll',
+#                             'overflowX': 'scroll'
+#                         },
+#                     ),
+#
+#                     # html.Div( #div for the graph
+#                     #     dcc.Graph(id='CVgraph')
+#                     # ),
+#                 ],
+#
+#                 className='col-lg-3',
+#             )
+# go.Scatter(
+#     name='Infected',
+#     # events qty
+#     x=np.arange(year[0], year[1]),
+#     # year
+#     y=dff_A,
+#
+#     mode='bar',
+#     visible = True,
+#     marker={
+#         'symbol': 'circle',
+#         'size': 5,
+#         'color': '#eb1054'
+#         },
+#     hoverlabel={
+#         'bgcolor': '#FFF',
+#     },
+# ),
+# go.Scatter(
+#     name='Not Infected',
+#     # events qty
+#     x=np.arange(year[0], year[1]),
+#     # year
+#     y=dff_B,
+#
+#     mode='lines',
+#     visible = True,
+#     marker={
+#         'symbol': 'circle',
+#         'size': 5,
+#         'color': '#C2FF0A'
+#         },
+#     hoverlabel={
+#         'bgcolor': '#FFF',
+#     },
+# ),
+#
 # #CREATE CV GRAPH
 # def create_CV(dff):
 #     data = go.Data([
@@ -322,118 +445,6 @@ def update_map(year, toggle):
 #     dff = pd.read_csv(datastring, header=None)
 #
 #     return (create_CV(dff))
-
-
-#YEAR GRAPH
-@app.callback(
-    Output('by_year', 'figure'),
-    [Input('date-slider', 'value')]
-)
-def by_year(year):
-    dff = df[(df['year'] >= year[0]) & (df['year'] <=year[1])]
-    # dfff = dff[dff['classification']=='empty']
-    classification =['Infected', 'Not Infected']
-    for classes in classification:
-    #     dfff = dfff.append(dff[dff['classification'] == classes], ignore_index=True)
-        dff_counted = dff.groupby(['year','classification'] , as_index=False).count()
-    dff_A = dff_counted[dff_counted['classification'] == 'Infected']['number']
-    dff_B = dff_counted[dff_counted['classification'] == 'Not Infected']['number']
-    y_min = dff_counted['number'].min()
-    y_max = dff_counted['number'].max()
-
-    data = go.Data([
-        go.Bar(
-            name='Infected',
-            x=np.arange(year[0], year[1]),
-            y=dff_A,
-            visible = True,
-            hoverlabel={
-                'bgcolor': '#FFF',
-            },
-            marker_color='#407438'
-        ),
-        go.Bar(
-            name='Not Infected',
-            # events qty
-            x=np.arange(year[0], year[1]),
-            # year
-            y=dff_B,
-            visible = True,
-            hoverlabel={
-                'bgcolor': '#FFF',
-            },
-            marker_color = '#65BC22'
-        ),
-                # go.Scatter(
-                #     name='Infected',
-                #     # events qty
-                #     x=np.arange(year[0], year[1]),
-                #     # year
-                #     y=dff_A,
-                #
-                #     mode='bar',
-                #     visible = True,
-                #     marker={
-                #         'symbol': 'circle',
-                #         'size': 5,
-                #         'color': '#eb1054'
-                #         },
-                #     hoverlabel={
-                #         'bgcolor': '#FFF',
-                #     },
-                # ),
-                # go.Scatter(
-                #     name='Not Infected',
-                #     # events qty
-                #     x=np.arange(year[0], year[1]),
-                #     # year
-                #     y=dff_B,
-                #
-                #     mode='lines',
-                #     visible = True,
-                #     marker={
-                #         'symbol': 'circle',
-                #         'size': 5,
-                #         'color': '#C2FF0A'
-                #         },
-                #     hoverlabel={
-                #         'bgcolor': '#FFF',
-                #     },
-                # ),
-    ])
-
-    layout = go.Layout(
-        xaxis={
-            #'autorange': True,
-            'color': '#000000',
-            'title': 'year',
-            'range': [year[0], year[1]],
-            'dtick': 1
-            },
-        yaxis={
-            #'autorange': True,
-            'color': '#000000',
-            'title': 'Number of Cases',
-            'range': [y_min, y_max],
-            #'dtick': 5
-            },
-        margin={
-            'l': 0,
-            'b': 0,
-            't': 0,
-            'r': 0
-            },
-            hovermode='closest',
-            paper_bgcolor='#F0E4E3',
-            plot_bgcolor='#F0E4E3',
-            autosize = True,
-            )
-
-    return go.Figure(
-        data=data,  # 54b4e4
-        layout=layout
-        )
-
 
 
 # Run dash server
